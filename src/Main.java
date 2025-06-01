c759f8d3fedf44e6f047dc33c0c6661860a30bde
Suresh Lagishetti <cjitsjn@gmail.com>
2025-06-01 18:08:32 +0530

    Initial commit: Simple Java app

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// src/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5, b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
